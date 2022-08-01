44c8974b35c454ab1a9731d108c078b5085111ca
kiku <753750568abc@gmail.com>
2022-08-01 15:22:20 +0800

    update home router

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,8 @@
 <template>
   <nav>
-    <router-link :to="{ name: 'cyt' }">cyt</router-link> |
+    <router-link :to="{ name: 'cyt-index' }">cyt</router-link> |
     <router-link :to="{ name: 'iris' }">iris</router-link> |
     <router-link :to="{ name: 'ting' }">ting</router-link>
-    <div onclick="debounce(fn, 1000)"></div>
   </nav>
 </template>
 
